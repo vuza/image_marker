ba8644716af1de12327053b162e7f353417f1c03
Marlon Alagoda <marlon@alagoda.at>
2016-01-15 00:23:32 +0100

    implemented real async c++ module test method

--- server.js
+++ server.js
@@ -9,4 +9,9 @@ server.use(function (req, res, next) {
     next();
 });
 
+var im_processor = require('./im_processor/build/Release/im_processor');
+im_processor.test(function(r){
+    console.log(r);
+});
+
 server.listen(3991);
