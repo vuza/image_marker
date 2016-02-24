75194e41818fc884cb5ec5b50196005cecce7d5d
Marlon Alagoda <marlon@alagoda.at>
2016-02-24 18:34:27 +0100

    replacing frontend config

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,6 +1,6 @@
 define([], function(){
     return {
-        api: process.env.API || 'http://localhost:3991/api/v1',
-        socket: process.env.SOCKET || 'http://localhost:3000'
+        api: 'http://localhost:3991/api/v1',
+        socket: 'http://localhost:3000'
     };
 });
