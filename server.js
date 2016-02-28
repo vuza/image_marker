d5764a7c338a1b09380f89ff22b72f365c94bd68
Marlon Alagoda <marlon@alagoda.at>
2016-02-28 20:49:32 +0100

    code cleanup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var router = require('./router'),
     imageController = require('./controller/imageController'),
     config = require('./config'),
     winston = require('winston'),
+    async = require('async'),
     server = require('express')();
 
 /**
@@ -14,15 +15,9 @@ winston.add(winston.transports.File, { filename: config.logging.location + '/' +
 winston.info('Starting server...');
 
 /**
- * Load images
+ * Register middlewares
  */
-imageController.loadImages(function(err){
-    if(err)
-        winston.error('Could not find or create image folder');
-
-    /**
-     * Init and start server
-     */
+var registerMiddlewares = function(cb){
     server.use(function (req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -32,7 +27,27 @@ imageController.loadImages(function(err){
 
     server.use('/api/v1/', router);
 
+    if(cb) cb(null);
+};
+
+/**
+ * Start server
+ */
+var startServer = function(cb){
     server.listen(config.listenPort, function(){
         winston.info('Started listening at ' + config.listenPort);
+
+        if(cb) cb(null);
     });
+};
+
+async.series([
+    imageController.loadImages,
+    registerMiddlewares,
+    startServer
+], function(err){
+    if(err)
+        winston.error('Error while init and starting server: ' + JSON.stringify(err));
+    else
+        winston.info('Finished starting, ready to rock');
 });
