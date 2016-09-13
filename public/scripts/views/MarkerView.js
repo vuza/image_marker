f59d4af2be37cec9f0270320c14fabe6c9828bed
Marlon Alagoda <marlon@alagoda.at>
2016-09-13 21:44:28 +0200

    typo

--- public/scripts/views/MarkerView.js
+++ public/scripts/views/MarkerView.js
@@ -38,7 +38,7 @@ define(['tpl!templates/markerView.tpl', 'Marionette', 'd3', 'randomcolor', 'path
             var imageOffset = $image.offset();
             var absoluteClickPosition = {x: event.clientX, y: event.clientY};
             var scrollTop = $(window).scrollTop();
-            var scrollLeft = $(window).scrollLeft()
+            var scrollLeft = $(window).scrollLeft();
             var relativeClickPosition = {
                 x: absoluteClickPosition.x - imageOffset.left + scrollLeft,
                 y: absoluteClickPosition.y - imageOffset.top + scrollTop
@@ -48,7 +48,7 @@ define(['tpl!templates/markerView.tpl', 'Marionette', 'd3', 'randomcolor', 'path
             var superpixelsize = settingsChannel.request('superpixelsize');
             var compactness = settingsChannel.request('compactness');
             var thr_col_val = settingsChannel.request('thr_col_val');
-            var label = settingsChannel.request('label');
+            var label = settingsChannel.request('label')
 
             // Lock view and show loading
             notificationChannel.trigger('loading:show');
