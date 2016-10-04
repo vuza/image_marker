347603e247586edd73d74a0e349858ee495b8bd3
andreas <andreas.wittmann.aw@gmail.com>
2016-10-04 10:27:08 +0200

    change test

--- im_processor/src/lib/main_test_im_processor_api.cpp
+++ im_processor/src/lib/main_test_im_processor_api.cpp
@@ -15,20 +15,20 @@ int main(int argc, char *argv[])
     Im_processor_api* api = new Im_processor_api();
 
     api->prepareImg("/mnt/Data/git/image_marker/public/images/image.1.jpg", 100, 10, 25);
-    //string jsonMatrix1 = api->getImageMatrix("/mnt/Data/git/image_marker/public/images/image.1.jpg", 100, 10, 25);
-    //string compressedMatrix1 = api->getImageMatrixCompressed("/mnt/Data/git/image_marker/public/images/image.1.jpg", 100, 10, 25);
+    string jsonMatrix1 = api->getImageMatrix("/mnt/Data/git/image_marker/public/images/image.1.jpg", 100, 10, 25);
+    string compressedMatrix1 = api->getImageMatrixCompressed("/mnt/Data/git/image_marker/public/images/image.1.jpg", 100, 10, 25);
 
-    api->prepareImg("/mnt/Data/git/image_marker/public/images/image.2.jpg", 100, 10, 25);
+    //api->prepareImg("/mnt/Data/git/image_marker/public/images/image.2.jpg", 100, 10, 25);
     //string jsonMatrix2 = api->getImageMatrix("/mnt/Data/git/image_marker/public/images/image.2.jpg", 100, 10, 25);
     //string compressedMatrix2 = api->getImageMatrixCompressed("/mnt/Data/git/image_marker/public/images/image.2.jpg", 100, 10, 25);
 
-    api->prepareImg("/mnt/Data/git/image_marker/public/images/image.3.jpg", 100, 10, 25);
+    //api->prepareImg("/mnt/Data/git/image_marker/public/images/image.3.jpg", 100, 10, 25);
     //string jsonMatrix3 = api->getImageMatrix("/mnt/Data/git/image_marker/public/images/image.3.jpg", 100, 10, 25);
     //string compressedMatrix3 = api->getImageMatrixCompressed("/mnt/Data/git/image_marker/public/images/image.3.jpg", 100, 10, 25);
-    /*
+
     std::cout << "jsonMatrix1 Length: " << jsonMatrix1.length() << std::endl;
     std::cout << "compressedMatrix1 Length: " << compressedMatrix1.length() << std::endl;
-
+    /*
     std::cout << "jsonMatrix2 Length: " << jsonMatrix2.length() << std::endl;
     std::cout << "compressedMatrix2 Length: " << compressedMatrix2.length() << std::endl;
 
